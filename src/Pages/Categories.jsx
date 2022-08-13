4772a3f5f86625c5cb113cccd474ce0fe5ec296c
Evgeniy Smetanin <esmet91@yandex.ru>
2022-08-13 22:39:29 +0300

    ...

--- src/Pages/Categories.jsx
+++ src/Pages/Categories.jsx
@@ -19,6 +19,8 @@ const Categories = ({ books }) => {
   const booksSort =
     category === ''
       ? []
+      : category === 'ALL'
+      ? books
       : books
           .filter((obj) => {
             if (obj.category === category || obj.subCategory === category) {
